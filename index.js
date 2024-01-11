f63cf3164252868ab9327723ad4fb314c6777c33
kajauvais <uvais2104@gmail.com>
2024-01-11 12:44:13 +0530

    add Qr-code master

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const qrcode = require('qrcode');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+
+app.get('/',(req,res)=>{
+    res.render("index")
+})
+app.post('/result',(req,res)=>{
+    let gettingdata = req.body.qrcodetext;
+    qrcode.toDataURL(gettingdata,(err,data)=>{
+        res.render("result",{kaja:data})
+    })
+    
+})
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'index.html'));
+// })
+
+// app.get('/result',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'result.html'));
+// })
+app.listen(2104)
